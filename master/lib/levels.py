205c76dafed3312c28979f04581a97e1e03101ac
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-04-26 06:18:09 -0300

    sapitizado

--- master/lib/levels.py
+++ master/lib/levels.py
@@ -1,5 +1,5 @@
 LEVELS = [
-    ('nivel1.tga',      'prueba',),
+    ('nivel1.tga',      'llegada',),
     ('phil_1.tga',      'Jungle - 1',),
     ('phil_7.tga',      'Jungle - 2',),
     ('tim_2.tga',       'Jungle - 3',),
@@ -17,20 +17,4 @@ LEVELS = [
     ('phil_5.tga',      'Moon - 3',),
     ('phil_10.tga',     'Moon - 4',),
     ('boss_1.tga',      'Boss',),
-    
-    #('phil_1.tga',      'Jungle - 1',), # jungle - easy
-    #('phil_7.tga',      'Jungle - 2',), # jungle - medium 
-    #('tee_1.tga',       'Jungle - 3',), # jungle - hard
-    #('phil_2.tga',      'Jungle - 4',), # jungle - hard
-    
-    #('fydo_1.tga',      'Volcano - 1',), # volcano - easy
-    #('tim_1.tga',       'Volcano - 2',), # volcano - medium
-    #('pekuja_2.tga',    'Volcano - 3',), # volcano - medium
-    #('phil_8.tga',      '* Bonus *',), # volcano bonus 
-    
-    #('pekuja_1.tga',    'Moon - 1',), # moon - medium
-    #('phil_5.tga',      'Moon - 2',), # moon - medium 
-    #('phil_9.tga',      'Moon - 3',), # moon - medium
-    #('boss_1.tga',      'Boss',), # moon - boss
-    
-    ]
+]
