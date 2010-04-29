62d774edaaa8669b22231c78b4c788b9445dfa81
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-04-29 17:25:48 -0300

    eslito

--- master/lib/level.py
+++ master/lib/level.py
@@ -24,7 +24,7 @@ def load_level(fname):
             l[1][y][x] = g
             l[2][y][x] = b
     return l
-    
+
 def load_tiles(fname):
     img = pygame.image.load(fname).convert_alpha()
     w,h = img.get_width()/TW,img.get_height()/TH
@@ -45,11 +45,11 @@ def load_images(dname):
             parts = a.split('.')
             if parts[0] == '' or len(parts) != 2: continue
             #print 'loading image',a
-            
+
             key = relative_root + parts[0]
-            img = pygame.image.load(os.path.join(root,a)).convert_alpha() 
+            img = pygame.image.load(os.path.join(root,a)).convert_alpha()
             r[key] = img
-            
+
             if 'left' in key:
                 key = key.replace('left','right')
                 #print 'creating flipped image',key
@@ -60,18 +60,18 @@ def load_images(dname):
                 #print 'creating flipped image',key
                 img = pygame.transform.flip(img,1,0)
                 r[key] = img
-            
+
     return r
-    
+
 class Tile:
     def __init__(self,n,pos):
         self.image = n
         pass
-    
+
 def pre_load():
     Level._tiles = load_tiles(data.filepath('tiles.tga'))
     Level._images = load_images(data.filepath('images'))
-    
+
 class Level:
     def __init__(self,game,fname,parent):
         self.game = game
@@ -88,14 +88,14 @@ class Level:
         else:
             self.title = os.path.basename(self.fname)
         self.data = load_level(fname)
-        
+
         #self.images = load_images(data.filepath('images'))
         self.images = Level._images
         img = pygame.Surface((1,1)).convert_alpha()
         img.fill((0,0,0,0))
         self.images[None] = img
         for n in xrange(0,len(self._tiles)): self.images[n] = self._tiles[n]
-        
+
         import tiles
         self._images = []
         for m in xrange(0,IROTATE):
@@ -109,24 +109,24 @@ class Level:
             for n1,n2 in tiles.TREPLACE:
                 r[n1] = self.images[n2]
             self._images.append(r)
-        
+
         self.size = len(self.data[0][0]),len(self.data[0])
-        
+
         self._bkgr_fname = None
         self.set_bkgr('1.png')
         self.bkgr_scroll = pygame.Rect(0,0,1,1)
-        
+
         self.view = pygame.Rect(0,0,SW,SH)
         self.bounds = pygame.Rect(0,0,self.size[0]*TW,self.size[1]*TH)
-        
+
         self.sprites = []
         self.player = None
         self.frame = 0
         self.codes = {}
-        
+
         # initialize all the tiles ...
         self.layer = [[None for x in xrange(0,self.size[0])] for y in xrange(0,self.size[1])]
-         
+
         for y in xrange(0,self.size[1]):
             l = self.data[0][y]
             for x in xrange(0,self.size[0]):
@@ -137,34 +137,34 @@ class Level:
                     #if n == 0: n = n1
                 if not n: continue
                 tiles.t_put(self,(x,y),n)
-        
+
         for y in xrange(0,self.size[1]):
             l = self.data[2][y]
             for x in xrange(0,self.size[0]):
                 n = l[x]
                 if not n: continue
                 codes.c_init(self,(x,y),n)
-                
+
         #just do a loop, just to get things all shined up ..
         self.status = None
         self.loop()
         self.status = '_first'
         self.player.image = None
         self.player.exploded = 30
-                
+
     def set_bkgr(self,fname):
         if self._bkgr_fname == fname:
             return
         self._bkgr_fname = fname
         self.bkgr = pygame.image.load(data.filepath(os.path.join('bkgr',fname))).convert()
 
-                
+
     def run_codes(self,r):
         #r.clamp_ip(self.bounds)
         for y in xrange(r.top/TH,r.bottom/TH):
             if y < 0 or y >= self.size[1]: continue
             for x in xrange(r.left/TW,r.right/TW):
-                if x < 0 or x >= self.size[0]: continue 
+                if x < 0 or x >= self.size[0]: continue
                 if (x,y) not in self.codes:
                     n = self.data[2][y][x]
                     if n == 0: continue
@@ -172,7 +172,7 @@ class Level:
                     if s == None: continue
                     s._code = (x,y)
                     self.codes[(x,y)] = s
-                
+
     def get_border(self,dist):
         r = pygame.Rect(self.view)
         r.x -= dist
@@ -180,13 +180,13 @@ class Level:
         r.w += dist*2
         r.h += dist*2
         return r
-        
+
     def paint(self,screen):
         self.view.clamp_ip(self.bounds)
-        
+
         #TODO: optimize sometime, maybe ...
         #screen.fill((0,0,0))
-        
+
         v = self.view
         dh = (screen.get_height()-v.h)/2
         if dh:
@@ -198,7 +198,7 @@ class Level:
             screen.fill((0,0,0),(SW-dw,0,dw,SH))
         _screen = screen
         screen = screen.subsurface(dw,dh,v.w,v.h)
-        
+
         bg = self.bkgr
         r = pygame.Rect(0,0,bg.get_width(),bg.get_height())
         if r.w > self.bounds.w:
@@ -212,36 +212,36 @@ class Level:
         #that picked out the center of the surface ...
         #DrPetter likes the top ...
         r.y = 0
-        
+
         bg = bg.subsurface(r)
-        
+
         vw = bg.get_width() - self.view.w
         bw = max(1,self.bounds.w - self.view.w)
         x = vw*(self.view.x-self.bounds.x)/bw
-        
+
         vw = bg.get_height() - self.view.h
         bw = max(1,self.bounds.h - self.view.h)
         y = vw*(self.view.y-self.bounds.y)/bw
-        
+
         dx,dy = -x,-y
-        
+
         if self.bkgr_scroll.y == 0:
-            
+
             screen.blit(bg,(dx,dy))
         else:
-        
+
             #dx += self.bkgr_scroll.x*self.frame
             dy += self.bkgr_scroll.y*self.frame
             #dx = dx % bg.get_width()
             dy = dy % bg.get_height()
-        
+
             screen.blit(bg,(dx,dy))
             screen.blit(bg,(dx,dy-bg.get_height()))
-        
+
         v = self.view
-        
+
         bg = self.data[1]
-        
+
         images = self._images[self.frame%IROTATE]
         for y in xrange(v.top-v.top%TH,v.bottom,TH):
             for x in xrange(v.left-v.left%TW,v.right,TW):
@@ -251,30 +251,30 @@ class Level:
                 s = self.layer[y/TH][x/TW]
                 if s != None and s.image:
                     screen.blit(images[s.image],(x-v.left,y-v.top))
-                
+
         for s in self.sprites:
             if s.exploded == 0:
                 screen.blit(images[s.image],(s.rect.x-s.shape.x-v.x,s.rect.y-s.shape.y-v.y))
             else:
                 w=images[s.image].get_width()
-                top=s.rect.y-s.shape.y-v.y-images[s.image].get_height() / 2 * s.exploded; 
+                top=s.rect.y-s.shape.y-v.y-images[s.image].get_height() / 2 * s.exploded;
                 for ty in xrange(0,images[s.image].get_height()):
                     screen.blit(images[s.image],(s.rect.x-s.shape.x-v.x,top+ty*(1+s.exploded)),(0,ty,w,1))
-                    
-                    
+
+
         screen = _screen
         self.paint_text(screen)
-        
-            
+
+
         self.game.flip()
-    
+
     def update(self,screen):
         return self.paint(screen)
-    
+
     def loop(self):
         #record the high scores
         self.game.high = max(self.game.high,self.game.score)
-        
+
         if self.status != '_first':
             # start up some new sprites ...
             r = self.get_border(INIT_BORDER)
@@ -282,35 +282,35 @@ class Level:
             self.run_codes(pygame.Rect(r.right-TW,r.y,TW,r.h)) #right
             self.run_codes(pygame.Rect(r.x,r.bottom-TH,r.w,TH)) #bottom
             self.run_codes(pygame.Rect(r.x,r.y,TW,r.h)) #left
-            
+
             # grab the current existing sprites
-            # doing this avoids a few odd situations 
+            # doing this avoids a few odd situations
             sprites = self.sprites[:]
-            
+
             # mark off the previous rect
             for s in sprites:
                 s.prev = pygame.Rect(s.rect)
-            
+
             # let the sprites do their thing
             for s in sprites:
                 if s.loop == None: continue
                 s.loop(self,s)
-            
+
             # re-calculate the groupings
             groups = {}
             for s in sprites:
                 for g in s.groups:
                     if g not in groups: groups[g] = []
                     groups[g].append(s)
-        
+
             # hit them sprites! w-tsh!
             for s1 in sprites:
                 for g in s1.hit_groups:
                     if g not in groups: continue
                     for s2 in groups[g]:
                         if not s1.rect.colliderect(s2.rect): continue
-                        s1.hit(self,s1,s2) 
-                
+                        s1.hit(self,s1,s2)
+
             # hit walls and junk like that
             for s in sprites:
                 if not len(s.groups): continue
@@ -323,12 +323,12 @@ class Level:
                         if not t.hit_groups.intersection(s.groups): continue
                         dist = abs(t.rect.centerx-s.rect.centerx)+abs(t.rect.centery-s.rect.centery)
                         hits.append([dist,t])
-                            
+
                 hits.sort()
                 for dist,t in hits:
                     if not t.rect.colliderect(s.rect): continue
                     t.hit(self,t,s)
-                    
+
             # remove inactive sprites
             border = self.get_border(DEINIT_BORDER)
             for s in sprites:
@@ -343,21 +343,21 @@ class Level:
                             print 'error in code GC',s._code
                             continue
                         del self.codes[s._code]
-                            
+
             #pan the screen
             if self.player != None:
                 self.player.pan(self,self.player)
-            
+
         # more frames
         self.frame += 1
         if (self.frame % FPS) == 0:
             pass
             #print self.player.rect.bottom
-            #print '' 
+            #print ''
             #print 'frame:',self.frame
             #print 'sprites:',len(self.sprites)
             #print 'codes:',len(self.codes)
-            
+
         #handle various game status'
         if self.status == '_first':
             if self.player.exploded:
@@ -374,44 +374,44 @@ class Level:
                 self.game.music_play('finish')
                 next = menu.Transition(self.game,self.parent)
                 return menu.Pause(self.game,'Felicidades!!!', next)
-                
+
             else:
                 self.game.music_play('lvlwin', 1)
                 l2 = Level(self.game, self.fname,self.parent)
                 next = menu.Transition(self.game, l2)
                 return menu.Pause(self.game, 'Bien Hecho!', next)
-                
-                
+
+
         elif self.status == 'dead':
             if self.game.lives:
                 self.game.lives -= 1
                 return menu.Transition(self.game, Level(self.game, self.fname,
                     self.parent))
             else:
-                next = menu.Transition(self.game,self.parent) 
+                next = menu.Transition(self.game,self.parent)
                 return menu.Pause(self.game, 'Juego Terminado', next)
         elif self.status == 'transition':
             self.status = None
             return menu.Transition(self.game,self)
-            
-            
+
+
 
     def event(self,e):
         #if e.type is KEYDOWN and e.key in (K_ESCAPE,):
         if e.type is USEREVENT and e.action == 'exit':
             next = menu.Transition(self.game,self.parent)
             return menu.Prompt(self.game,'Salir? S/N', next, self)
-        
+
         if self.player != None:
             self.player.event(self,self.player,e)
-            
-            
+
+
     def paint_text(self,screen):
         fnt = self.game.fonts['level']
         pad = 4
-        
+
         top_y = pad
-        
+
         blit = screen.blit
         text = '%05d' % self.game.score
         c = (0, 0, 0)
@@ -428,7 +428,7 @@ class Level:
         c = (255, 255, 255)
         img = fnt.render(text, 1, c)
         blit(img,(x,y)) ; blit(img,(x,y))
-        
+
 #        text = 'LIVES: %d' % self.game.lives
 
         for i in xrange(self.game.lives):
@@ -446,7 +446,7 @@ class Level:
 #        c = (255,255,255)
 #        img = fnt.render(text,1,c)
 #        blit(img,(x,y)) ; blit(img,(x,y))
-                
+
         text = '%02d'%self.game.coins
         c = (0,0,0)
         img = fnt.render(text,1,c)
