c4252a76e71623610d3b23d123e182d574abf262
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-04-29 17:25:48 -0300

    regresión

--- master/lib/cnst.py
+++ master/lib/cnst.py
@@ -1,12 +1,11 @@
 from pygame.locals import *
 
-SW,SH = 320,240
-TW,TH = 16,16
-FPS = 99
+SW, SH = 320, 240
+TW, TH = 16, 16
+FPS = 60
 IROTATE = 64
-#FPS = 10
 
-TITLE = "Tokuah" #"Tokuah en el desafio de los ajat"
+TITLE = "Tokuah"
 
 SCALE2X = False  #use the scale2x scaler to make things look hi-res
 LOWRES = False #keep it in 320x240 mode
@@ -14,8 +13,8 @@ FULL = True #
 
 #INIT_BORDER = 100
 #DEINIT_BORDER = 200
-INIT_BORDER = TW*2
-DEINIT_BORDER = TW*8
+INIT_BORDER = TW * 2
+DEINIT_BORDER = TW * 8
 
 
 # Input for keyboard:
@@ -58,13 +57,12 @@ CODE_BOSS_PHASE2_BLOCK = 0xA2
 # Various constants:
 DOOR_DELAY = 20 # Delay when going through a door
 
-#HACK: to have this function handy without a bunch of module.sign () blah blah
+#HACK: to have this function handy without a bunch of module.sign() blah blah
 def sign(v):
     if v < 0: return -1
     if v > 0: return 1
     return 0
-    
+
 #HACK: some code to find out who's printing out trash
 #import sys
 #sys.stdout = None
-    
