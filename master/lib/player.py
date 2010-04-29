1e00968db4a0ce4c3d5855a59cd0ced930408dda
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-04-29 17:07:27 -0300

    mas rapidez

--- master/lib/player.py
+++ master/lib/player.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
 import pygame
 from pygame.locals import *
 
@@ -7,7 +10,7 @@ import sprites
 import tiles
 
 def init(g,r,n,*params):
-    s = sprite.Sprite3(g,r,'player/right',(6,8,15-6,27-8)) #(0,0,19,25))#(43-14,8,28,48))
+    s = sprite.Sprite3(g, r, 'player/right', (6, 8, 15 - 6, 27 - 8))
     s.rect.bottom = r.bottom
     s.rect.centerx = r.centerx
     s.groups.add('player')
@@ -36,32 +39,35 @@ def init(g,r,n,*params):
     s.kill = kill
     s.god_mode = False
     s.death_counter = -1
-    
+
+
     s._prev = pygame.Rect(s.rect)
     s._prev2 = pygame.Rect(s.rect)
     s.looking = False
-    
+
     sprite.init_bounds(g,s)
     sprite.init_view(g,s)
     sprite.init_codes(g,s)
     s.no_explode = False
-    
+
     return s
-    
-def event(g,s,e):
+
+def event(g, s, e):
     #print 'player.event',e
     if s.door_timer != None or s.exploded > 0:
         return
-    
-    if e.type is USEREVENT and e.action == 'jump' and s.standing != None and s.jumping == 0 and s.vy == 0:
+
+    if (e.type is USEREVENT and e.action == 'jump' and s.standing != None and s.
+            jumping == 0 and s.vy == 0):
         sprite.stop_standing(g,s)
         #s.vy = -1.8
         s.vy = -0.5
         s.jumping = 1.4
         g.game.sfx['jump'].play()
+
     if e.type is USEREVENT and e.action == 'stop-jump':
         s.jumping = 0
-        
+
     if e.type is USEREVENT and (e.action == 'up' or e.action == 'down'):
         if sprite.get_code(g,s,0,0) in DOOR_CODES:
             s.vx = 0
@@ -80,18 +86,18 @@ def event(g,s,e):
         else:
             sprites.bubble.init(g,s.rect,s,big=False)
         s.shooting = 10
-        
+
     if e.type is KEYDOWN and e.key == K_F10:
         powerup(g,s)
         s.god_mode = True
-        
+
     #if e.type is KEYDOWN and e.key == K_F12:
         #1/0
-        
-        
+
+
 def loop(g,s):
     s._prev2 = pygame.Rect(s.rect)
-    
+
     if s.death_counter > 0:
         s.groups = set()
         if not s.no_explode:
@@ -104,18 +110,18 @@ def loop(g,s):
         return
     if s.death_counter == 0:
         g.status = 'dead'
-        return 
-    
+        return
+
     if s.exploded > 0:
         if s.powered_up:
             s.image = 'player/right'
         else:
             s.image = 'splayer/right'
-        s.exploded -=1 
+        s.exploded -=1
         return
-        
 
-        
+
+
     sprite.apply_gravity(g,s)
     sprite.apply_standing(g,s)
 
@@ -133,23 +139,23 @@ def loop(g,s):
             return
 
     inpt = g.game.input
-    
+
     #if s.standing: s.rect.bottom = s.standing.rect.top
-    
+
     #check if we hit the ceiling
     if not s.jumping and s.vy < 0 and s.rect.y == s._prev.y:
         s.vy = 0
-        
+
     # We have universal input code now (>__>)
     #move by keyboard
     #keys = pygame.key.get_pressed()
-    
+
     if s.jumping:
         #print s.vy
         s.vy -= s.jumping
         s.jumping = max(0,s.jumping-0.2)
-    
-    inc = 0.5 
+
+    inc = 0.5
     mx = 2
     if inpt.right and s.vx < mx:
         s.vx += inc
@@ -160,17 +166,17 @@ def loop(g,s):
         s.facing = 'left'
     elif not inpt.left and s.vx < 0:    s.vx += inc
 
-    
+
     s._prev = pygame.Rect(s.rect)
 
     vx,vy = s.vx,s.vy
     s.rect.x += vx
     s.rect.y += sprite.myinc(g.frame,s.vy)
-    
-    
+
+
     #if keys[K_UP]: vy -= 1
     #if keys[K_DOWN]: vy += 1
-    
+
     if s.vy < 0:
         s.image = 'player/%s-jump' % (s.facing)
     elif s.shooting > 0:
@@ -200,7 +206,7 @@ def loop(g,s):
             s.powerup_transition -= 1
         elif not s.powered_up:
             s.image = 's' + s.image
-    
+
     s.looking = False
     if inpt.up:
         g.view.y -= 2
@@ -208,8 +214,8 @@ def loop(g,s):
     if inpt.down:
         g.view.y += 2
         s.looking = True
-    
-    n = sprite.get_code(g,s,0,0) 
+
+    n = sprite.get_code(g,s,0,0)
     if n == CODE_EXIT:
         g.status = 'exit'
     if n == CODE_DOOR_AUTO:
@@ -217,9 +223,9 @@ def loop(g,s):
         import door
         door.hit(g,(x,y),s)
 
-    
+
     #pan_screen(g,s)
-    
+
     #if (g.frame%FPS)==0: print 'player vy:',s.vy
 
     if hasattr(g, 'boss'):
@@ -233,10 +239,10 @@ def loop(g,s):
             g.status = 'exit'
             #pygame.mixer.music.load("
             #g.game.music.play('finish',1)
-    
-    
+
+
 def pan_screen(g,s):
-    # adjust the view 
+    # adjust the view
     border = pygame.Rect(s.rect)
     #pad = 100
     pad = (SW/2) - TW
@@ -248,13 +254,13 @@ def pan_screen(g,s):
         pad = TH * 2
     border.y -= pad
     border.h += pad * 2
-    
+
     dest = pygame.Rect(g.view)
     dest.top = min(dest.top,border.top)
     dest.right = max(dest.right,border.right)
     dest.bottom = max(dest.bottom,border.bottom)
     dest.left = min(dest.left,border.left)
-    
+
     dx,dy = dest.x-g.view.x,dest.y-g.view.y
     #mx,my = 6,6
     mx = max(2,abs(s._prev2.x-s.rect.x))
@@ -263,25 +269,24 @@ def pan_screen(g,s):
     if abs(dy) > my: dy = sign(dy)*my
     g.view.x += dx
     g.view.y += dy
-    
+
 
 def powerup(g,s):
-
     if not s.powered_up:
         s.powerup_transition = 100
         s.powered_up = True
 
         if hasattr(g.game, 'powerup'):
             g.game.powerup = True
-    
+
 
 def damage(g,s):
     if s.god_mode:
         return
-    
+
     if s.door_timer != None:
         return
-    
+
     if s.powered_up:
         g.game.sfx['pop'].play()
         s.powerup_transition = 100
@@ -290,7 +295,7 @@ def damage(g,s):
             g.game.powerup = False
     elif s.powerup_transition == 0 and s.flash_counter == 0:
         s.kill(g,s)
-        
+
 def kill(g, s, no_explode=False):
     if hasattr(g.game, 'powerup'):
         g.game.powerup = False
@@ -298,4 +303,4 @@ def kill(g, s, no_explode=False):
     s.no_explode = no_explode
     g.game.music_play('death', 1)
     s.death_counter = int(FPS * 2.25)
-    
+
