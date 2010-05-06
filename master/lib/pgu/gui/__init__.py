2d9e5ea0c6e5cb95d21dbe39edfd6c372a9e4fcb
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-05-06 16:28:57 -0300

    reparado leveledit

--- master/lib/pgu/gui/__init__.py
+++ master/lib/pgu/gui/__init__.py
@@ -17,7 +17,7 @@ from area import SlideBox, ScrollArea, List
 from form import Form
 from group import Group
 
-from basic import Spacer, Color, Label, Image
+from basic import Spacer, Color, Label, Image, parse_color
 from button import Icon, Button, Switch, Checkbox, Radio, Tool, Link
 from input import Input, Password
 from keysym import Keysym
@@ -27,5 +27,6 @@ from misc import ProgressBar
 
 from menus import Menus
 from dialog import Dialog, FileDialog
+from textarea import TextArea
 
 from deprecated import Toolbox, action_open, action_setvalue, action_quit, action_exec
