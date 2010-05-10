907544f27df8ae0bff2a2d503a4bf9be43621a2f
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-05-10 16:59:38 -0300

    probando grvedad

--- master/lib/sprite.py
+++ master/lib/sprite.py
@@ -11,17 +11,17 @@ class Sprite:
         self.shape = pygame.Rect(0, 0, TW, TH)
         self.exploded = 0
         self.loop = None
-        
+
         self.hit_groups = set()
         self.hit = None
         self.groups = set()
-        
+
         # needed for gravity to work / not work ... :)
         self.standing = None
         self.active = True
         self.deinit = deinit
         self.auto_gc = True
-        
+
 def Sprite2(g, r, n):
     s = Sprite(r, n)
     img = g.images[n]
@@ -39,11 +39,11 @@ def Sprite3(g, r, n, shape):
 
 def apply_gravity(g, s):
     if s.standing != None:
-        s.vy = 0 
+        s.vy = 0
         return
-    s.vy += 0.3
+    s.vy += 0.4
     s.vy = min(s.vy, 16)
-    
+
 def apply_standing(g,s):
     if s.standing == None: return
     if not s.standing.active:
@@ -57,18 +57,18 @@ def apply_standing(g,s):
         stop_standing(g, s)
         s.rect.y += 1 #throw on a bit o' gravity
         return
-        
+
 def stop_standing(g,s):
     if hasattr(s.standing,'carrying'):
         if s in s.standing.carrying:
             s.standing.carrying.remove(s)
     s.standing = None
-    
+
 def deinit(g,s):
     if hasattr(s,'standing'):
         stop_standing(g,s)
-    
-    
+
+
 def init_bounds(g,s):
     x,y = s.rect.centerx/TW,s.rect.centery/TH
     min_x,min_y,max_x,max_y = x,y,x,y
@@ -79,31 +79,31 @@ def init_bounds(g,s):
     min_x += 1
     min_y += 1
     g.bounds = pygame.Rect(min_x*TW,min_y*TH,(max_x-min_x)*TW,(max_y-min_y)*TH)
-    
+
     g.view.w = min(SW,g.bounds.w)
     g.view.h = min(SH,g.bounds.h)
-    
+
     #if g.bounds.w < SW:
         #print 'uh oh, g.bounds.w < SW',g.bounds.w
     #if g.bounds.h < SH:
         #print 'uh oh, g.bounds.h < SH',g.bounds.h
-    
+
 def init_view(g,s):
     g.view.centerx = s.rect.centerx
     g.view.centery = s.rect.centery
     s.pan(g,s)
-    
+
 def init_codes(g,s):
     g.view.clamp_ip(g.bounds)
     border = g.get_border(INIT_BORDER)
     g.run_codes(border)
 
-    
+
 def sign(v):
     if v < 0: return -1
     if v > 0: return 1
     return 0
-    
+
 def get_code(g,s,ix,iy):
     #dx,dy get taken down to their signed component
     dx,dy = sign(ix),sign(iy)
