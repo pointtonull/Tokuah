9730cbad31a3c3e1de9b003c8d8d29f5537b2cbe
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-05-10 00:45:00 -0300

    velocidad

--- master/lib/capsule.py
+++ master/lib/capsule.py
@@ -5,10 +5,8 @@ from pygame.locals import *
 
 import sprite
 
-def init(g,r):
-    #print 'new capsule bubble'
-
-    s = sprite.Sprite3(g,r,'captured-generic', (0,0,16,16))
+def init(g, r):
+    s = sprite.Sprite3(g, r, 'captured-generic', (0, 0, 32, 32))
 
     s.rect.centerx = r.centerx
     s.rect.centery = r.centery
@@ -19,15 +17,15 @@ def init(g,r):
     s.loop = loop
 
     s.life = 300
-    
+
     s.vx = (g.game.random % 10) / 10.0 - 0.5#random.random() - 0.5
-    s.vy = -0.5
+    s.vy = -1.0
     s.x = float(s.rect.x)
     #s.y = float(s.rect.y)
-    
+
     s.carrying = []
     s.prev = pygame.Rect(s.rect)
-    
+
     g.game.sfx['capsule'].play()
 
     return s
@@ -41,41 +39,35 @@ def loop(g,s):
     dx = int(s.x-s.rect.x)
     #s.rect.x = s.x
     #s.rect.y = s.y
-    
+
     #dx = sprite.myinc(g.frame,s.vx)
     dy = sprite.myinc(g.frame,s.vy)
-    
+
     s.rect.x += dx
     s.rect.y += dy
 
     if s.life == 0:
         die(g,s)
     s.life -= 1
-    
+
     for b in s.carrying:
         b.rect.x += dx
         b.rect.y += dy
-    
-    
-#def hit(g,a,b):
-    ##die(g,a)
-    #pass
+
 
 def die(g,s):
     g.game.sfx['pop'].play()
     s.active = False
+
     for b in s.carrying:
         sprite.stop_standing(g,b)
-        #print b.standing
-    # release the enemy?
-
-
 
 
 def hit(g,a,b):
     #print 'youve been platformed!'
-    if not hasattr(b,'standing'): return
-    
+    if not hasattr(b,'standing'):
+        return
+
     r,aprev,cur,prev = a.rect,a.prev,b.rect,b.prev
     #if prev.bottom <= r.top and cur.bottom > r.top:
     if prev.bottom <= aprev.top and cur.bottom > r.top:
