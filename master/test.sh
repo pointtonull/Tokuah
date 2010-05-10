2a8a6663e8a8e8e387b8fccc8e5866b50ade3ea0
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-05-10 00:20:47 -0300

    doble sized

--- master/test.sh
+++ master/test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 while true; do
+    git checkout master
+    ./run_game.py -full -nosound
+    git checkout experimental
     ./run_game.py -full -nosound
     echo "##############################"
     sleep 5
