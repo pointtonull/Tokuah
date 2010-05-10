ee9efa657af46487ed2b71fadeb5887134919032
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-05-10 01:25:31 -0300

    vuelta al estado inicial

--- master/test.sh
+++ master/test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#while true; do
-    pushing
-    git checkout master
-    ./run_game.py -full -nosound
-    git checkout experimental
+while true; do
+#    pushing
+#    git checkout master
+#    ./run_game.py -full -nosound
+#    git checkout experimental
     ./run_game.py -full -nosound
     echo "##############################"
-#    sleep 5
-#done
+    sleep 5
+done
