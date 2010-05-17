55fcdd525f4c887f435faa1149e35fc9ba917685
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-05-17 02:56:59 -0300

    actualización

--- master/lib/sprites/door.py
+++ master/lib/sprites/door.py
@@ -6,79 +6,76 @@ from cnst import *
 import sprite
 import player
 
-def init(g,r,n,hidden = False,*params):
-    s = sprite.Sprite3(g, r, 'door-1', (0, 0, 32, 48)) #3
-    s.rect.centerx = r.centerx
-    s.rect.centery = r.centery - (48 - 32) / 2
-    s.loop = loop
-    s.hit = sprite_hit
-    s.frame = 0
-    s.open = None
-    s.hit_groups.add('player')
-    s.hidden = hidden
-    if hidden:
-        s.image = None
-    g.sprites.insert(0, s)
-    return s
+class Door(sprite.Sprite3):
+    def __init__(self, game, rect, name, hidden=False, *args):
+        sprite.Sprite3.__init__(self, game, rect, 'door-1', (0, 0, 32, 48))
+        self.rect.centerx = rect.centerx
+        self.rect.centery = rect.centery - (48 - 32) / 2
+        self.hit = sprite_hit
+        self.frame = 0
+        self.open = None
+        self.hit_groups.add('player')
+        self.hidden = hidden
+        if hidden:
+            self.image = None
+        self.game.sprites.insert(0, self)
 
-def loop(g, s):
-    if s.hidden:
-        s.image = None
-        return
-    if s.open > 0:
-        s.image = 'door-open'
-        s.open -= 1
-        return
-    elif (s.frame % 256) > 192:
-        s.image = 'door-2'
-    else:
-        s.image = 'door-1'
-    s.open = None
-    s.frame += 1
+    def loop(self):
+        if self.hidden:
+            self.image = None
+            return
+        if self.open > 0:
+            self.image = 'door-open'
+            self.open -= 1
+            return
+        elif (self.frame % 256) > 192:
+            self.image = 'door-2'
+        else:
+            self.image = 'door-1'
+        self.open = None
+        self.frame += 1
 
-def sprite_hit(g, a, b):
-    b.current_door = a
+    def sprite_hit(self, a, b):
+        b.current_door = a
+            
+    def hit(self, pos, b):
         
-def hit(g, pos, b):
-    
-    cx,cy = pos
-    
-    import sprite
-    #n_code = sprite.get_code(g,a,1,0)
-    dx = 1
-    while g.data[2][cy][cx + dx] in DOOR_CODES:
-        dx += 1
-    n_code = g.data[2][cy][cx + dx]
-    
-    if n_code == 0:
-        return
-    
-    layer = g.data[2]
-    
-    w, h = g.size
-    xx = cx
-    yy = cy
+        cx, cy = pos
+        
+        dx = 1
+        while self.game.data[2][cy][cx + dx] in DOOR_CODES:
+            dx += 1
+        n_code = g.data[2][cy][cx + dx]
+        
+        if n_code == 0:
+            return
+        
+        layer = self.game.data[2]
+        
+        w, h = self.game.size
+        xx = cx
+        yy = cy
 
-    for y in xrange(h):
-        for x in xrange(w):
-            if layer[y][x] in DOOR_CODES and layer[y][x - 1] == n_code:
-                xx = x
-                yy = y
-    
-    #t = g.layer[yy][xx]
-    rect = pygame.Rect(xx * TW, yy * TH, TW, TH)
-    s = b
-    s.rect.centerx = rect.centerx
-    s.rect.bottom = rect.bottom
-    if s.standing != None:
-        sprite.stop_standing(g, s)
-    
-    sprite.init_bounds(g, s)
-    sprite.init_view(g, s)
-    sprite.init_codes(g, s)
-    s.prev = pygame.Rect(s.rect)
-    s._prev = pygame.Rect(s.rect)
-    
-    g.status = 'transition'
-    
-    g.game.sfx['door'].play()
+        for y in xrange(h):
+            for x in xrange(w):
+                if layer[y][x] in DOOR_CODES and layer[y][x - 1] == n_code:
+                    xx = x
+                    yy = y
+        
+        #t = g.layer[yy][xx]
+        self.rect = pygame.Rect(xx * TW, yy * TH, TW, TH)
+        self = b
+        self.rect.centerx = rect.centerx
+        s.rect.bottom = rect.bottom
+        if s.standing != None:
+            sprite.stop_standing(g, s)
+        
+        sprite.init_bounds(g, s)
+        sprite.init_view(g, s)
+        sprite.init_codes(g, s)
+        s.prev = pygame.Rect(s.rect)
+        s._prev = pygame.Rect(s.rect)
+        
+        g.status = 'transition'
+        
+        g.game.sfx['door'].play()
